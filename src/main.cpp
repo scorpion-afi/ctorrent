f072ebfc9cb54f7c3ca69adafdfed4246a55711b
Sergey Sizonov <s.sizonov@samsung.com>
2017-03-15 13:25:08 +0300

    add POSIX message queue

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,89 @@
+#include <iostream>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <cstring>
 
-#include <semaphore.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/socket.h>
 
-#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-const std::string semaphore_name = "/tdm-video-playback";
+#include <mqueue.h>
+
+#include <vector>
+#include <thread>
+
+const std::string mq_name = "/my_queue";
+
+void read_thread_body( void )
+{
+	mqd_t mq;
+	mq_attr _mq_attr;
+	int ret;
+
+	mq = mq_open( mq_name.c_str(), O_RDONLY );
+	if( mq < 0 )
+	{
+		char buf[256];
+		std::cout << "an attempt to open message queue failed: " <<
+				strerror_r( errno, buf, sizeof(buf) ) << std::endl;
+
+		return;
+	}
+
+	mq_getattr( mq, &_mq_attr );
+
+	std::vector<char> message( _mq_attr.mq_msgsize );
+
+	while( 1 )
+	{
+		ret = mq_receive( mq, message.data(), message.size(), nullptr );
+		if( ret < 0 )
+		{
+			if( errno == EINTR )
+				continue;
+
+			char buf[256];
+			std::cout << "an attempt to dequeue a message failed: " <<
+					strerror_r( errno, buf, sizeof(buf) ) << std::endl;
+
+			goto exit;
+		}
+
+		break;
+	}
+
+	std::cout << "a message has been received: \"" << message.data() <<
+			"\", amount of data received (in bytes): " << ret << std::endl;
+
+exit:
+	mq_close( mq );
+	mq_unlink( mq_name.c_str() );
+}
 
 int main( void )
 {
-	pid_t child_pid;
-	sem_t* sem;
+	mqd_t mq;
+	std::string message = "hi, cruel world.";
 
-	/* an initial value is 0 */
-	sem = sem_open( semaphore_name.c_str(), O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR, 0 );
-	if( sem == SEM_FAILED )
+	mq = mq_open( mq_name.c_str(), O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR, nullptr );
+	if( mq < 0 )
 	{
-		perror( "an attempt to create named semaphore failed" );
+		char buf[256];
+		std::cout << "an attempt to create message queue failed: " <<
+				strerror_r( errno, buf, sizeof(buf) ) << std::endl;
+
 		return 1;
 	}
 
-	child_pid = fork();
-	if( child_pid == -1 )
-	{
-		perror( "an attempt to fork failed" );
-		return 1;
-	}
+	std::thread read_thread(read_thread_body);
 
-	/* child related code */
+	mq_send( mq, message.data(), message.size(), 0 );
+	read_thread.join();
 
-	if( !child_pid )
-	{
-		sem = sem_open( semaphore_name.c_str(), O_RDWR );
-		if( sem == SEM_FAILED )
-		{
-			perror( "[client] an attempt to open named semaphore failed" );
-			return 1;
-		}
-
-		while( 1 )
-		{
-			int ret;
-
-			/* wait until parent notices us */
-			ret = sem_wait( sem );
-			if( ret < 0 && errno == EINTR )
-				continue;
-			else
-				break;
-		}
-
-		sem_close( sem );
-		sem_unlink( semaphore_name.c_str() );
-
-		return 0;
-	}
-
-	/* parent related code */
-
-	/* we got to allow the client to open a semaphore :-) */
-	sleep(1);
-
-	sem_post( sem );
-	sem_close( sem );
-	sem_unlink( semaphore_name.c_str() );
+	mq_close( mq );
+	mq_unlink( mq_name.c_str() );
 
 	return 0;
 }
