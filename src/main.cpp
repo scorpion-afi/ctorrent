a93f787bec3e0226fdd44b52ec55e6cad8870bde
Sergey Sizonov <s.sizonov@samsung.com>
2017-03-28 14:45:26 +0300

    check pthread_cond_broadcast

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 #include <stdio.h>
-#include <cstring>
-#include <string>
 
 #include <unistd.h>
-#include <errno.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <pthread.h>
 
-#include <sys/socket.h>
-#include <wait.h>
-
-#include <boost/regex.hpp>
+int some_check = 1;
+pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
 
+void* thread_func( void* )
+{
+	pthread_mutex_lock( &mtx );
+
+	while( some_check )
+	{
+		pthread_cond_wait( &cond_var, &mtx );
+		std::cout << pthread_self() << ", " <<some_check << std::endl;
+
+		if( some_check ) some_check = 0;
+	}
+
+	pthread_mutex_unlock( &mtx );
+
+	return nullptr;
+}
 
 int main( void )
 {
-	std::string line;
-	boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
+	pthread_t threads[4] = { 0 };
 
-	while( std::cin )
-	{
-		std::getline( std::cin, line );
-		boost::smatch matches;
+	for( auto& thr : threads )
+		pthread_create( &thr, nullptr, thread_func, nullptr );
 
-		if( boost::regex_match( line, matches, pat ) )
-			std::cout << matches[2] << std::endl;
+	sleep( 2 );
+	//pthread_cond_signal( &cond_var );  // cause ONE thread being moved from cond-queue to mtx-queue
+	pthread_cond_broadcast( &cond_var ); // cause ALL threads being moved from cond-queue to mtx-queue
 
-	}
+	for( auto& thr : threads )
+		pthread_join( thr, nullptr );
 }
