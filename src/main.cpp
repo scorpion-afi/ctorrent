e64cfb84dbab0b6cf122bc5d4d8d8af494dc9390
Sergey Sizonov <s.sizonov@samsung.com>
2017-04-13 16:50:04 +0300

    split serialization to store & load

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/version.hpp>
+#include <boost/serialization/split_member.hpp>
 
 const std::string archive_name( "archive.txt" );
 
@@ -77,17 +78,33 @@ private:
 	friend class boost::serialization::access;
 
 	template< class Archive >
-	void serialize( Archive& ar, const unsigned int version )
+	void save( Archive& ar, const unsigned int version ) const
 	{
 		/* an insane way to serialize base part of derived object */
-		ar & boost::serialization::base_object<base>( *this );
+		ar & boost::serialization::base_object<base>(*this);
 
-		std::cout << "serialization for derived object, version: " << version << "...\n";
+		std::cout << "store for derived object, version: " << version << "...\n";
+
 		ar & hi;
 
+		/* we got to write to archive anyway */
+		ar & dd;
+	}
+
+	template< class Archive >
+	void load( Archive& ar, const unsigned int version )
+	{
+		ar & boost::serialization::base_object<base>( *this );
+
+		std::cout << "load for derived object, version: " << version << "...\n";
+		ar & hi;
+
+		/* we got to check as we're loading object from archive (to support old archives) */
 		if( version == 1 )
 			ar & dd;
 	}
+
+	BOOST_SERIALIZATION_SPLIT_MEMBER()
 
 	/* added for 0-th version */
 	int hi;
@@ -98,9 +115,30 @@ private:
 
 BOOST_CLASS_VERSION( derived, 1 );
 
-
 int main( void )
 {
+	{
+		std::ofstream ofs( archive_name );
+		boost::archive::text_oarchive text_output_archive( ofs );
+
+		/* we got to register derived class to be able to save polymorphic pointers */
+		text_output_archive.register_type( static_cast<derived*>(nullptr) );
+
+		std::shared_ptr<base> bs = std::make_shared<derived>( 26 );
+		std::cout << *bs;
+
+		text_output_archive << bs.get();
+
+		/* I don't understand what's the purpose of save_binary function */
+		//derived der( 4 );
+		//text_output_archive.save_binary( &der, sizeof(der) );
+
+		//int a = 25;
+		//text_output_archive.save_binary( &a, 4 );
+
+		//text_output_archive.save_binary( "hi cruel world.", sizeof("hi cruel world.") );
+	}
+
 	std::ifstream ifs( archive_name );
 	boost::archive::text_iarchive text_input_archive( ifs );
 
