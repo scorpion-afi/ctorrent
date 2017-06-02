2cedf9bf23b0537ee625d3463e4610c14e83c03f
Sergey Sizonov <s.sizonov@samsung.com>
2017-06-02 16:09:08 +0300

    AF_UNIX server/client model

--- src/main.cpp
+++ src/main.cpp
@@ -1,289 +1,255 @@
 #include <iostream>
-#include <streambuf>
+#include <array>
+#include <string>
+#include <vector>
 
-#include <list>
-
-#include <boost/archive/binary_iarchive.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-
-#include <boost/serialization/list.hpp>
-
-#include <boost/serialization/base_object.hpp>
-#include <boost/serialization/version.hpp>
-#include <boost/serialization/split_member.hpp>
+#include <cstring>
 
 #include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 
-class base
+
+const std::string socket_name = "my_socket";
+
+void server( void )
 {
-public:
-	base( int i, double d, char c ) : i(i), d(d), c(c)
-    {
-		int_list.push_back( 25 ); /* int_list is default-initialized */
-    }
+	sockaddr_un s_addr;
+	int listen_socket = -1;
+	int ret;
 
-	base() : i(0), d(0.0), c('a') {} /* int_list is default-initialized */
+	sockaddr_un peer_addr;
+	socklen_t addr_len;
+	int peer_socket = -1;
 
-	base( const base& ref ) = default;
-	base( base&& ref ) = delete;
+	std::vector<char> request_data(4096);
+	int ret_size;
 
-	base& operator=( const base& ref ) = default;
-	base& operator=( base&& ref ) = delete;
-
-	virtual ~base() {}
-
-	virtual void show( std::ostream& cout = std::cout ) const
+	/* create a stream-based, reliable, bidirectional socket within AF_UNIX protocols family */
+	listen_socket = socket( AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0 );
+	if( listen_socket < 0 )
 	{
-		cout << " base object: " << "i: " << i << ", d: " << d << ", c: " << c << std::endl;
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] socket(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		return;
 	}
 
-private:
+	std::memset( &s_addr, 0, sizeof(s_addr) );
 
-	friend std::ostream& operator<<( std::ostream& cout, const base& bs );
+	s_addr.sun_family = AF_UNIX;
+	std::memcpy( s_addr.sun_path, socket_name.c_str(),
+			std::min(socket_name.size() + 1, sizeof(s_addr.sun_path)) );	/* string is char array */
 
-	/* an 'access' class should have an access to our private method 'serialize' */
-	friend class boost::serialization::access;
-
-	template< class Archive >
-	void serialize( Archive& ar, const unsigned int version )
+	/* bind socket to some local socket address */
+	ret = bind( listen_socket, reinterpret_cast<sockaddr*>(&s_addr), sizeof(s_addr) );
+	if( ret < 0 )
 	{
-		/* load order is equal to store order */
-		ar & i;
-		ar & d;
-		ar & c;
-		ar & int_list;
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] bind(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+
+		close( listen_socket );
+		return;
 	}
 
-	int i;
-	double d;
-	char c;
+	std::cout << "[server] the socket has been bound to the local socket address.\n";
 
-	std::list<int> int_list;
-};
+	/* mark socket as a passive socket so it can be used to accept incoming connection requests, via accept()
+	 * socket is still bound to socket address I specified above */
+	ret = listen( listen_socket, 2 );
+	if( ret < 0 )
+	{
+		int err = errno;
+		std::array<char, 256> buf;
 
-std::ostream& operator<<( std::ostream& cout, const base& bs )
-{
-	bs.show( cout );
-	return cout;
+		std::cout << "[server] [error] listen(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		goto finish;
+	}
+
+	addr_len = sizeof(peer_addr);
+	std::memset( &peer_addr, 0, sizeof(peer_addr) );
+
+	std::cout << "[server] wait for incoming connection requests..." << std::flush;
+
+	/* accept incoming connection request from listen_socket's queue of pending connection requests
+	 * return accepted socket that is in connected (to the remote peer) state */
+	peer_socket = accept4( listen_socket, reinterpret_cast<sockaddr*>(&peer_addr), &addr_len, SOCK_CLOEXEC );
+	if( peer_socket < 0 )
+	{
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] accept4(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		goto finish;
+	}
+
+	if( addr_len > sizeof(peer_addr) )
+		std::cout << "[server] [warning] a peer socket address has been truncated.\n";
+
+	std::cout << " success.\n" "[server] a new connection has been accepted, wait for request..." << std::flush;
+
+	/* try to get up to request_data.size() bytes from the socket */
+	ret_size = recv( peer_socket, &request_data.front(), request_data.size(), 0 );
+	if( ret_size < 0 )
+	{
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] recv(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		goto finish;
+	}
+
+	std::cout << "[server] a received request: (size: " << ret_size << ")\n ";
+
+	for( char ch : request_data )
+	 std::cout << ch;
+	std::cout << std::endl;
+
+	std::cout << "[server] send a reply..." << std::flush;
+
+	/* send data to the socket (this socket is connected to the remote socket) */
+	ret_size = send( peer_socket, "A.K., Moscow.", sizeof("A.K., Moscow."), MSG_NOSIGNAL );
+	if( ret_size < 0 )
+	{
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] send(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		goto finish;
+	}
+
+	std::cout << " success.\n";
+	std::cout << "[server] the end.\n";
+
+finish:
+	close( peer_socket );
+	close( listen_socket );
+	unlink( socket_name.c_str() );
 }
 
-class derived : public base
+void client( void )
 {
-public:
-	explicit derived( int num ) : base(3, 2.5, 'b'), hi(num), dd(8)	{}
+	sockaddr_un server_addr;
+	int cl_socket;
+	int ret;
 
-	derived() = default;
+	std::string request = "If you know Why it doesn't matter How.";
+	std::vector<char> reply( 4096 );
 
-	derived( const derived& ref ) = default;
-	derived( derived&& ref ) = delete;
-
-	derived& operator=( const derived& ref ) = default;
-	derived& operator=( derived&& ref ) = delete;
-
-	void show( std::ostream& cout = std::cout) const override
+	cl_socket = socket( AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0 );
+	if( cl_socket < 0 )
 	{
-		cout << " derived object:\n ";
-		base::show( cout );
-		cout << "  hi: " << hi << ", dd: " << dd << std::endl;
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[client] [error] socket(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		return;
 	}
 
-private:
+	std::memset( &server_addr, 0, sizeof(server_addr) );
 
-	friend class boost::serialization::access;
+	server_addr.sun_family = AF_UNIX;
+	std::memcpy( server_addr.sun_path, socket_name.c_str(),
+			std::min(socket_name.size() + 1, sizeof(server_addr.sun_path)) );
 
-	template< class Archive >
-	void save( Archive& ar, const unsigned int version ) const
+	/* try to connect socket to the remote socket address,
+	 * send a connection request for remote socket which listens for connection requests */
+	ret = connect( cl_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr) );
+	if( ret < 0 )
 	{
-		/* an insane way to serialize base part of derived object */
-		ar & boost::serialization::base_object<base>(*this);
+		int err = errno;
+		std::array<char, 256> buf;
 
-		ar & hi;
+		std::cout << "[client] [error] connect(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
 
-		/* we got to write to archive anyway */
-		ar & dd;
+		close( cl_socket );
+		return;
 	}
 
-	template< class Archive >
-	void load( Archive& ar, const unsigned int version )
+	sockaddr_un cl_socket_addr;
+	socklen_t addr_len = sizeof(cl_socket_addr);
+
+	std::memset( &cl_socket_addr, 0, sizeof(cl_socket_addr) );
+	getpeername( cl_socket, reinterpret_cast<sockaddr*>(&cl_socket_addr), &addr_len );
+
+	if( addr_len > sizeof(cl_socket_addr) )
+		std::cout << "[client] [warning] a peer socket address has been truncated.\n";
+	else
+		std::cout << "[client] connection to the server has been established (peer name: "
+		    << cl_socket_addr.sun_path << "), send request..." << std::flush;
+
+	ret = send( cl_socket, request.c_str(), request.size() + 1, MSG_NOSIGNAL );
+	if( ret < 0 )
 	{
-		ar & boost::serialization::base_object<base>( *this );
+		int err = errno;
+		std::array<char, 256> buf;
 
-		ar & hi;
+		std::cout << "[client] [error] send(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
 
-		/* we got to check as we're loading object from archive (to support old archives) */
-		if( version == 1 )
-			ar & dd;
+		close( cl_socket );
+		return;
 	}
 
-	BOOST_SERIALIZATION_SPLIT_MEMBER()
+	std::cout << " success\n" "[client] wait for answer..." << std::flush;
 
-	/* added for 0-th version */
-	int hi;
-
-	/* added for 1-st version */
-	double dd;
-};
-
-BOOST_CLASS_VERSION( derived, 1 );
-
-
-/* raw memory based streambuffer implementation
- *
- * streambuffer implementation with std::vector<char> as an associated character sequence,
- * controlled character sequences aren't used */
-class raw_memory_stream_buff : public std::streambuf
-{
-public:
-	raw_memory_stream_buff() :
-		read_offset(0) { buff.reserve(memory_reserv); }
-
-	/* initialize an associated character sequence by copy of data */
-	explicit raw_memory_stream_buff( const std::vector<char>& data ) :
-		buff(data), read_offset(0) {}
-
-	/* The next functions provide interface to read/write data directly,
-	 * not over streambuf interface, it can be useful, e.g. for socket communication */
-
-	/* get */
-	std::vector<char> upload( void ) const { return buff; }
-	const std::vector<char>& upload_ref( void ) const { return buff; }
-
-	/* set */
-	void load( const std::vector<char>& data )
+	ret = recv( cl_socket, &reply.front(), reply.size(), 0 );
+	if( ret < 0 )
 	{
-		buff = data;
-		read_offset = 0;
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[client] [error] recv(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+
+		close( cl_socket );
+		return;
 	}
 
-private:
+	std::cout << "[client] a received request: (size: " << ret << ")\n ";
 
-	/* don't support copy semantic */
-	raw_memory_stream_buff( const raw_memory_stream_buff& ref ) = delete;
-	raw_memory_stream_buff& operator=( const raw_memory_stream_buff& ref ) = delete;
+	for( char ch : reply )
+	 std::cout << ch;
+	std::cout << std::endl;
 
-	/* these function will be invoked for every symbol to write/read as I've not called
-	 * setg(), setp() functions */
-	int_type overflow( int_type ch ) override;
-	int_type uflow( void ) override;
+	std::cout << "[client] the end.\n";
 
-
-	const int memory_reserv = 4096;
-
-	/* associated character sequence */
-	std::vector<char> buff;
-
-	unsigned int read_offset;
-};
-
-raw_memory_stream_buff::int_type raw_memory_stream_buff::uflow( void )
-{
-	raw_memory_stream_buff::int_type ret;
-
-	/* I don't reset vector after reading has been completed,
-	 * 'cause it's not a circle buffer implementation */
-	if( buff.begin() + read_offset >= buff.end() )
-		return traits_type::eof();
-
-	ret = traits_type::to_int_type( *(buff.begin() + read_offset) );
-	read_offset++;
-
-	return ret;
-}
-
-raw_memory_stream_buff::int_type raw_memory_stream_buff::overflow( int_type ch )
-{
-	if( ch == traits_type::eof() )
-		return traits_type::eof();
-
-	buff.push_back( ch );
-
-	return ch;
+	close( cl_socket );
 }
 
 
 int main( void )
 {
 	pid_t child_pid;
-	int sockets[2];
-	int ret;
-
-	/* create stream-based connected sockets pair in PF_LOCAL domain */
-	ret = socketpair( AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, sockets );
-	if( ret < 0 )
-	{
-		std::cout << "an attempt to create socketpair failed.\n";
-		return 1;
-	}
 
 	child_pid = fork();
 	if( child_pid < 0 )
 	{
 		std::cout << "an attempt to fork() failed.\n";
-		for( int fd : sockets )
-			close( fd );
-
 		return 1;
 	}
 
-	/* after fork child process has sockets[2] fds which refer to the same socket objects,
-	 * as parent refers to */
-
 	/* parent process */
 	if( child_pid )
-	{
-		raw_memory_stream_buff raw_stream_buf;
-		boost::archive::binary_oarchive raw_ar( raw_stream_buf );
-
-		std::shared_ptr<base> bs = std::make_shared<derived>( 26 );
-		std::cout << "object to transfer:\n" << *bs;
-
-		std::cout << "--------serialization start---------\n";
-
-		/* we got to register derived class to be able to save polymorphic pointers */
-		/* TODO: we have another way to do this, but now I don't wanna spent time to figure out
-		 *       how to do that */
-		raw_ar.register_type( static_cast<derived*>(nullptr) );
-		raw_ar << bs.get();
-
-		std::cout << "-------serialization finish---------\n";
-
-		const std::vector<char>& sered_obj = raw_stream_buf.upload_ref();
-
-		ret = send( sockets[0], &sered_obj.front(), sered_obj.size(), 0 );
-		if( ret < 0 )
-		{
-			std::cout << "an attempt to send() failed.\n";
-			return 1;
-		}
-
-		usleep( 20000 );
-	}
+		server();
 	else
 	{
-		char data[4096];
-		int size;
+		/* wait until server is ready */
+		usleep( 20000 );
 
-		size = recv( sockets[1], data, sizeof(data), 0 );
-		if( size < 0 )
-		{
-			std::cout << "an attempt to recv() failed.\n";
-			return 1;
-		}
-
-		raw_memory_stream_buff raw_stream_buf( std::vector<char>( &data[0], &data[size] ) );
-		boost::archive::binary_iarchive raw_ar( raw_stream_buf );
-
-		derived* received_obj = nullptr;
-
-		std::cout << "\n--------deserialization start-----------\n";
-
-		raw_ar >> received_obj;
-
-		std::cout << "---------deserialization finish---------\n";
-
-		std::cout << "received object:\n";
-		std::cout << *received_obj;
+		client();
 	}
 }
