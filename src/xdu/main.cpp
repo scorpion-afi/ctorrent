a2fe2f187669a6aaaf8b467b340a42a9b19b0d45
sergs <ivan0ivanov0@mail.ru>
2017-07-07 14:29:14 +0300

    implement background process
    Signed-off-by: sergs <ivan0ivanov0@mail.ru>

--- src/xdu/main.cpp
+++ src/xdu/main.cpp
@@ -2,8 +2,37 @@
 #include <config.h>
 
 #include <iostream>
+#include <array>
+#include <fstream>
+
+#include <cstring>
+#include <unistd.h>
+#include <errno.h>
+
 
 int main( void )
 {
-	std::cout << "hi cruel world.\n";
+	std::ofstream file( "/home/sergs/xdu.log" );
+	int res;
+
+	std::cout.rdbuf( file.rdbuf() );
+
+	std::cout << "I'm process with pid: " << getpid() << std::endl;
+	std::cout << "invoke daemon...\n";
+
+	/* make fork syscall and terminate parent process inside */
+	res = daemon( 0, 0 );
+	if( res < 0 )
+	{
+		int err = errno;
+		std::array<char, 256> buf;
+
+		std::cout << "[server] [error] socket(): " <<
+				strerror_r( err, &buf.front(), buf.size() ) << std::endl;
+		return 1;
+	}
+
+	/* we're in a background process, gratz */
+
+	std::cout << "I'm process with pid: " << getpid() << std::endl;
 }
