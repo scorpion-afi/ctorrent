24d7e8060ef920b132eb5dfca43eba74f047f8c0
sergs <ivan0ivanov0@mail.ru>
2017-08-25 15:20:37 +0300

    move to the text stream based log sink instead of syslog based.
    It may get a lot of time to provide syslog support for
    Android, so I decided if syslog is needed for Android,
    I'll add such support, later...
    
    Several env variables were added, so to be aware
    how to use them 'env.txt' file were provided.
    
    Signed-off-by: sergs <ivan0ivanov0@mail.ru>

--- src/xdu/main.cpp
+++ src/xdu/main.cpp
@@ -1,50 +1,72 @@
 
 #include <config.h>
 
+#include <stdlib.h>
+
 #include <iostream>
+#include <fstream>
 
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 
+#include <boost/core/null_deleter.hpp>
+
 #include <boost/log/sinks.hpp>
-#include <boost/log/sinks/sink.hpp>
-#include <boost/log/sinks/syslog_backend.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+
 
 namespace logging = boost::log;
-namespace sinks = logging::sinks;
+namespace sinks = boost::log::sinks;
 
-// Complete sink type
-typedef sinks::synchronous_sink< sinks::syslog_backend > sink_t;
+typedef sinks::synchronous_sink< sinks::text_ostream_backend > sink_t;
 
-void init_native_syslog()
+/* the log file within PWD */
+std::string log_file_name = "xdu.log";
+
+void init_boost_log( void )
 {
-    boost::shared_ptr< logging::core > core = logging::core::get();
+  /* create and initiate stream based sink backend */
+  boost::shared_ptr<sinks::text_ostream_backend> stream_log_backend =
+      boost::make_shared<sinks::text_ostream_backend> ();
 
-    // Create a backend
-    boost::shared_ptr< logging::sinks::syslog_backend > backend(new sinks::syslog_backend(
-    		logging::keywords::facility = logging::sinks::syslog::user,
-			logging::keywords::use_impl = logging::sinks::syslog::udp_socket_based
-    ));
+  if( getenv ("XDU_LOG_TTY") )
+    /* we can't allow to deallocate std::clog object :) */
+    stream_log_backend->add_stream( boost::shared_ptr<std::ostream>( &std::clog, boost::null_deleter() ) );
+  if( getenv ("XDU_LOG_FILE") )
+    stream_log_backend->add_stream( boost::make_shared<std::fstream>( log_file_name ) );
 
-    // Set the straightforward level translator for the "Severity" attribute of type int
-    backend->set_severity_mapper(sinks::syslog::direct_severity_mapping< int >("Severity"));
+  if( getenv ("XDU_DBG") )
+    stream_log_backend->auto_flush( true );
 
-    // Wrap it into the frontend and register in the core.
-    // The backend requires synchronization in the frontend.
-    core->add_sink(boost::make_shared< sink_t >(backend));
-    
-    core->set_filter
-    (
-        logging::trivial::severity >= logging::trivial::info
-    );
+  /* get reference to logging core */
+  boost::shared_ptr<logging::core> log_core = logging::core::get();
+
+  /* create sink frontend (linked with backend) and register it with logging core */
+  boost::shared_ptr<sink_t> sink = boost::make_shared<sink_t>( stream_log_backend );
+  log_core->add_sink( sink );
+
+  /* set default severity to initiate logging */
+  log_core->set_filter(  logging::trivial::severity >= logging::trivial::warning );
+
+  /* set up global filter for logging core */
+  if( getenv("XDU_LOG_ERR_LVL") )
+    log_core->set_filter( logging::trivial::severity >= logging::trivial::error );
+  if( getenv("XDU_LOG_DBG_LVL") )
+    log_core->set_filter( logging::trivial::severity >= logging::trivial::debug );
+
+  if( getenv("XDU_LOG") )
+    log_core->set_logging_enabled( true );
+  else
+    log_core->set_logging_enabled( false );
 }
 
 int main( void )
 {
-	std::cout << "wassup!\n";
+	init_boost_log();
 
-	init_native_syslog();
-	
+	BOOST_LOG_TRIVIAL( debug )  << "and this song is for any kid who gets picked on.";
 	BOOST_LOG_TRIVIAL( info )  << "lemme know when u get ready.";
+	BOOST_LOG_TRIVIAL( warning )  << "still got love for the streets.";
 }
