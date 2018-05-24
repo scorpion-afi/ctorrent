861eb8aaa5ad6b4c68f01d90e8618daaddb86788
sergs <ivan0ivanov0@mail.ru>
2018-05-24 16:54:34 +0300

    clean up a remote_server class

--- src/ctorrent/ctorrent_client/remote_server.h
+++ src/ctorrent/ctorrent_client/remote_server.h
@@ -8,14 +8,23 @@
 #ifndef REMOTE_SERVER_H
 #define REMOTE_SERVER_H
 
-#include "serializer_deserializer.h"
+#include <string>
+
 #include "remote_connection.h"
 
+/* this class describes connection to a remote server;
+ * can be used concurrently for receiving and sending from different threads
+ * without synchronization */
 class remote_server : public remote_connection
 {
 public:
-  remote_server() = delete;
   remote_server( int socket_fd, deserialized_objs_t& receive_storage, std::string identify_str );
+
+  remote_server( const remote_server& that ) = delete;
+  remote_server& operator=( const remote_server& that ) = delete;
+
+  remote_server( remote_server&& that ) = default;
+  remote_server& operator=( remote_server&& that ) = default;
 
   /* this function gets called if some objects were deserialized */
   void process_deserialized_objs( deserialized_objs_t objs ) override;
